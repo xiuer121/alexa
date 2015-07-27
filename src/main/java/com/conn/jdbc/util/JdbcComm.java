46273cbaf024a8b581e90fea82655b6e69f4f45a
lansq150616 <lansq150616@gxwsxx.com>
2015-07-27 16:06:35 +0800

    修改线程

--- src/main/java/com/conn/jdbc/util/JdbcComm.java
+++ src/main/java/com/conn/jdbc/util/JdbcComm.java
@@ -13,61 +13,23 @@ import com.po.HttpRespons;
 
 
 public class JdbcComm {
-
-	public static final String url = "jdbc:mysql://127.0.0.1/alexa";
-	public static final String name = "com.mysql.jdbc.Driver";
-	public static final String user = "root";
-	public static final String password = "123456";
-	public static Connection conn = null; 
-	public static Statement sm =null;
+ 
 	public static void main(String[] args){
-		try {
-			Class.forName(name);// 指定连接类型
-			conn = DriverManager.getConnection(url, user, password);// 获取连接
-			sm= conn.createStatement();
-			
-			for (int i = 0; i < 10; i++) {
-				new Thread(new Runnable() {
-					public void run() { 
-						String sql =" select site_url from top_millions limit 0,10000";
-						try {
-							
-						   ResultSet rs = sm.executeQuery(sql);
-						
-						  while(rs.next()){ 
-								HttpRequester request = new HttpRequester();
-								HttpRespons hr;
-								try {
-									System.out.println(rs.getString("site_url"));
-									hr = request.sendGet("http://"+rs.getString("site_url"));
-									 System.out.println(hr.getUrlString());     
-								} catch (IOException e) {
-									// TODO Auto-generated catch block
-									e.printStackTrace();
-								}   
-							   
-						  }
-						} catch (SQLException e) {
-							 
-							e.printStackTrace();
-						}
-					}
-					}).start();
+		int start = 0 ;
+		int end = 0;
+		for(int i=0;i<10;i++){ 
+		
+			if(i==0){
+				start=0;
+				end = i+1*100000;
+			}else{
+				end = i*100000;
 			}
-		
-			 
-		} catch (Exception e) {
-			e.printStackTrace();
+			MyThread myThread = new MyThread(start, end);
+			myThread.start();
+			start=i*10000;
+			
 		}
-		
-//		try {
-//			conn.close();
-//			sm.close();
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-	
 	}
 
 	 
--- src/main/java/com/conn/jdbc/util/MyThread.java
+++ src/main/java/com/conn/jdbc/util/MyThread.java
@@ -1,8 +1,14 @@
 package com.conn.jdbc.util;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+
+import com.po.HttpRequester;
+import com.po.HttpRespons;
 
 public class MyThread extends Thread {
 	public static final String url = "jdbc:mysql://127.0.0.1/alexa";
@@ -12,21 +18,54 @@ public class MyThread extends Thread {
 	public static Connection conn = null;
 	public static Statement sm = null;
 
+	// 定义读取的起始点
+	private long start;
+	// 定义读取的结束点
+	private long end;
+
+	public MyThread(long start, long end) {
+		this.start = start;
+		this.end = end;
+	}
+
 	@Override
 	public void run() {
-		for (int i = 0; i < 10; i++) {
+
+		try {
+			Class.forName(name);
+			conn = DriverManager.getConnection(url, user, password);// 获取连接
+			sm = conn.createStatement();
+			String sql = " select site_url from top_millions limit " + start
+					+ "," + end;
+
+			ResultSet rs = sm.executeQuery(sql);
+			while (rs.next()) {
+				HttpRequester request = new HttpRequester();
+				HttpRespons hr;
+				try {
+					System.out.println(rs.getString("site_url"));
+					hr = request.sendGet("http://" + rs.getString("site_url"));
+					System.out.println(hr.getUrlString());
+				} catch (IOException e) {
+
+					e.printStackTrace();
+				}
+
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}// 指定连接类型
+		finally {
+
 			try {
-				Class.forName(name);
-				conn = DriverManager.getConnection(url, user, password);// 获取连接
-				sm = conn.createStatement();
-				String sql =" select site_url from top_millions limit "+i+","+i*10000;
-				
-				i=i*10000;
-			} catch (Exception e) {
-
+				conn.close();
+				sm.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}// 指定连接类型
-
+			}
+			
 		}
 	}
 
